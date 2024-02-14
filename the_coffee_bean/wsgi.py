541a37cdaf97e62e01c3fabb91b9d394feef10b4
BanditCook <tanguy.lalexandre@gmail.com>
2024-02-14 17:32:58 +0000

    Created homepage with responsive

--- the_coffee_bean/wsgi.py
+++ the_coffee_bean/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_ado.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_coffee_bean.settings')
 
 application = get_wsgi_application()
