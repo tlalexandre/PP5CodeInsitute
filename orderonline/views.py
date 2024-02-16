3160aec9ab0fda7adf2a966382c2e7c2aea51732
BanditCook <tanguy.lalexandre@gmail.com>
2024-02-16 09:31:26 +0000

    Added urls, views and templates for Order Online

--- orderonline/views.py
+++ orderonline/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def order_online(request):
+    """ A view to return the order online page """
+    
+    return render(request, 'orderonline/orderonline.html')
