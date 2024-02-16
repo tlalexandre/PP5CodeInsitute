3b713caea625cf83e743ecbbc73a6c32a24a4750
BanditCook <tanguy.lalexandre@gmail.com>
2024-02-16 09:17:45 +0000

    Added urls and views for about and menu pages

--- home/views.py
+++ home/views.py
@@ -6,3 +6,14 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+    
+    return render(request, 'about/about.html')
+
+def menu(request):
+    """ A view to return the menu page """
+    
+    return render(request, 'menu/menu.html')
