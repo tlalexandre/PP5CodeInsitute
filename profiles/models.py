68489e02dc3e01764704498263a264bebf237ee5
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-12 11:35:57 +0000

    Added form profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=10)
+    country = models.CharField(max_length=50)
--- profiles/views.py
+++ profiles/views.py
@@ -1,30 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+from .models import Profile
 
 
 
 @login_required
 def profile(request):
-    """ Display the user's profile. """
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+            profile, created = Profile.objects.get_or_create(user=request.user)
+            profile.first_name = form.cleaned_data['first_name']
+            profile.last_name = form.cleaned_data['last_name']
+            profile.address = form.cleaned_data['address']
+            profile.city = form.cleaned_data['city']
+            profile.postal_code = form.cleaned_data['postal_code']
+            profile.country = form.cleaned_data['country']
+            profile.save()
+            return redirect('profile')
+    else:
+        profile, created = Profile.objects.get_or_create(user=request.user)
+        form = UserProfileForm(initial={
+            'first_name': profile.first_name,
+            'last_name': profile.last_name,
+            'address': profile.address,
+            'city': profile.city,
+            'postal_code': profile.postal_code,
+            'country': profile.country,
+        })
+    return render(request, 'profiles/profile.html', {'form': form})
 
-    template = 'profiles/profile.html'
-
-    return render(request, template)
-
-# @login_required
-# def order_history(request, order_number):
-#     order = get_object_or_404(Order, order_number=order_number)
-
-#     messages.info(request, (
-#         f'This is a past confirmation for order number {order_number}. '
-#         'A confirmation email was sent on the order date.'
-#     ))
-
-#     template = 'checkout/checkout_success.html'
-#     context = {
-#         'order': order,
-#         'from_profile': True,
-#     }
-
-#     return render(request, template, context)
