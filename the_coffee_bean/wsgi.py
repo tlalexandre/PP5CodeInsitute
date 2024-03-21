ec59282068176a91ab9cc28c1f59d48f08e20d42
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-21 14:01:07 +0000

    Added whitenoise

--- the_coffee_bean/wsgi.py
+++ the_coffee_bean/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_coffee_bean.settings')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
