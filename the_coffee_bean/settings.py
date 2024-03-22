23b7b95c827f36d15395b42ff6487a56e4669f9a
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-22 13:47:53 +0000

    Removed allauth middleware for deployment

--- the_coffee_bean/settings.py
+++ the_coffee_bean/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
