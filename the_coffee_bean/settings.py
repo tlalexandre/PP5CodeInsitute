f6e7e74bf1dc7d98870423829d2f27644a69bf7f
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-22 11:18:30 +0000

    Changed allowed hosts for deployement

--- the_coffee_bean/settings.py
+++ the_coffee_bean/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['the-coffee-bean.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['the-coffee-bean.herokuapp.com', 'the-coffee-bean-1b1bc662dda4.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
