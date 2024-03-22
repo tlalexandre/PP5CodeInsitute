4433973d090d1fb3a07373a8e7001d104fc43c9f
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-22 13:43:46 +0000

    Modified import env to fix Heroku deployement

--- the_coffee_bean/settings.py
+++ the_coffee_bean/settings.py
@@ -13,18 +13,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from .env import *
 
+# Check if the environment variable 'HEROKU' is set to 'true'
+if os.environ.get('HEROKU') != 'true':
+    # If it's not, we're not running on Heroku, so import the env module
+    from .env import *
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -150,13 +146,13 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
