5317917db563d45145d1c837baf802f929dc6ef5
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-24 11:30:51 +0000

    Added cache control

--- the_coffee_bean/settings.py
+++ the_coffee_bean/settings.py
@@ -201,6 +201,11 @@ MEDIA_URL = '/media/'
 
 if 'USE_AWS' in os.environ:
     # Bucket configuration
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'the-coffee-bean'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -218,6 +223,8 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
 
 
 # Default primary key field type
