8c0035419424364058bc181c6f6f41f781b250e4
BanditCook <tanguy.lalexandre@gmail.com>
2024-03-28 12:16:20 +0000

    Added 404 page

--- the_coffee_bean/urls.py
+++ the_coffee_bean/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('checkout/', include('checkout.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'the_coffee_bean.views.handler404'
